05df839e855bd22637937803b23d0ec2e00304d3
Madhav <madhav@pop-os.localdomain>
2020-03-29 22:47:59 +0530

    added database

---
+++ data.py
@@ -0,0 +1,9 @@
+import sqlite3
+import pandas as pd
+conn = sqlite3.connect('projectables.db')  
+c = conn.cursor()
+c.execute('''SELECT * FROM Project''')
+query=c.fetchall()
+# print(query)
+df = pd.DataFrame(query,columns=['PROJECT_ID','[S.NO.]','TITLE','CONTENT','OWNER_ID','COST','AUTHOR','RATING'])
+print(df)
