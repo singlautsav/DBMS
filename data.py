9eefe9d7306044e04e59a93c513ebb6b0db59066
Madhav <madhav@pop-os.localdomain>
2020-04-16 15:24:56 +0530

    added front-end

--- data.py
+++ data.py
@@ -1,9 +1,16 @@
 import sqlite3
 import pandas as pd
+from flask import Flask, render_template, Response, request, redirect, url_for,Markup,request
 conn = sqlite3.connect('projectables.db')  
 c = conn.cursor()
 c.execute('''SELECT * FROM Project''')
 query=c.fetchall()
-# print(query)
 df = pd.DataFrame(query,columns=['PROJECT_ID','[S.NO.]','TITLE','CONTENT','OWNER_ID','COST','AUTHOR','RATING'])
 print(df)
+app = Flask(__name__)
+# app._static_folder = "/static"
+@app.route('/',methods=['GET'])
+def index():
+   return(render_template('/product_list.html'))
+if __name__ == "__main__":
+   app.run(debug=True)
