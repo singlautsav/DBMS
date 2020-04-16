c76fc92b2bfe9cc54e7ffdad284cca9fb4288a49
Utsav Singla <utsav18321@iiitd.ac.in>
2020-04-17 01:54:00 +0530

    link the remaining pages

--- data.py
+++ data.py
@@ -39,18 +39,53 @@ def login():
 
 
 @app.route('/product_list',methods=['GET','POST'])
-def getProductList():
+def product_list():
    return render_template('/product_list.html')
 
 
 @app.route('/myCart',methods=['GET','POST'])
-def mycart():
+def cart():
    return render_template('/cart.html')
 
 
 @app.route('/checkout', methods = ['Get','POST'])
 def checkout():
    return render_template('/checkout.html')
+
+@app.route('/about', methods = ['Get','POST'])
+def about():
+   return render_template('/about.html')
+
+@app.route('/blog', methods = ['Get','POST'])
+def blog():
+   return render_template('/blog.html')
+
+
+@app.route('/Catagori', methods = ['Get','POST'])
+def Catagori():
+   return render_template('/Catagori.html')
+
+
+@app.route('/Confirmation', methods = ['Get','POST'])
+def Confirmation():
+   return render_template('/confirmation.html')
+
+
+@app.route('/contact', methods = ['Get','POST'])
+def contact():
+   return render_template('/contact.html')
+
+@app.route('/elements', methods = ['Get','POST'])
+def elements():
+   return render_template('/elements.html')
+
+@app.route('/main', methods = ['Get','POST'])
+def main():
+   return render_template('/main.html')
+
+@app.route('/single-blog', methods = ['Get','POST'])
+def single_blog():
+   return render_template('/single-blog.html')
 
 if __name__ == "__main__":
    app.run(debug=True)
