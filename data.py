5874b31103c9893b14043bfd51fc0a9f164f5553
Madhav <madhav@pop-os.localdomain>
2020-04-20 14:30:34 +0530

    Added single product

--- data.py
+++ data.py
@@ -4,6 +4,13 @@ from flask import Flask, render_template, Response, request, redirect, url_for,M
 
 
 app = Flask(__name__)
+def query_db(q):
+   conn = sqlite3.connect('projectables.db')
+   c = conn.cursor()
+   c.execute(q)
+   query=c.fetchall()
+   df = pd.DataFrame(query,columns=['PROJECT_ID','[S.NO.]','TITLE','CONTENT','OWNER_ID','COST','AUTHOR','RATING'])
+   return(df)
   
 # @app.route('/product_list',methods=['GET','POST'])
 @app.route('/',methods=['GET','POST'])
@@ -13,20 +20,19 @@ def index():
       search=request.form['search']
    except:
       print("dd")
-   conn = sqlite3.connect('projectables.db')
-   c = conn.cursor()
    q=str(f'''SELECT * FROM Project WHERE (TITLE LIKE "%{search}%") ORDER BY [S.NO.] DESC LIMIT 10''')
-   c.execute(q)
-   query=c.fetchall()
-   df = pd.DataFrame(query,columns=['PROJECT_ID','[S.NO.]','TITLE','CONTENT','OWNER_ID','COST','AUTHOR','RATING'])
-   print(df)
-   print(search)
+   df=query_db(q)
    return(render_template('/product_list.html',data=df))
 
 @app.route('/<idX>', methods=['GET','POST'])
 def onProductClick(idX):
    print(idX)
-   return (render_template('/single-product.html'))
+   q=str(f'''SELECT * FROM Project WHERE (TITLE = "{idX}")''')
+   df=query_db(q)
+   print(df)
+   for i in df:
+      print(df[i])
+   return (render_template('/single-product.html',data=df))
 
 @app.route('/index',methods = ['GET','POST'])
 def index_page():
@@ -45,12 +51,8 @@ def product_list():
       search=request.form['search']
    except:
       print("dd")
-   conn = sqlite3.connect('projectables.db')
-   c = conn.cursor()
    q=str(f'''SELECT * FROM Project WHERE (TITLE LIKE "%{search}%") ORDER BY [S.NO.] DESC LIMIT 10''')
-   c.execute(q)
-   query=c.fetchall()
-   df = pd.DataFrame(query,columns=['PROJECT_ID','[S.NO.]','TITLE','CONTENT','OWNER_ID','COST','AUTHOR','RATING'])
+   df=query_db(q)
    print(df)
    print(search)
    return(render_template('/product_list.html',data=df))
